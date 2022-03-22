7a4b31cea957941f13f1ebfff8fa7664feff8cfe
Daniel Ramirez ~ <danirg1414@outlook.com>
2022-03-22 10:08:54 -0500

    Client created

--- src/comm/Client.java
+++ src/comm/Client.java
@@ -1,8 +1,23 @@
 package comm;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException{
+		
+
+			while(true) {
+				Scanner scanner = new Scanner(System.in);
+				String line = scanner.nextLine()+"\n";
+				
+				Socket socket = new Socket("127.0.0.1", 6000);
+				
+			}
+			
+		
 		
 	}
 	
